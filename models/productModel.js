5a1a08acef5960fd2aeb97f32d8aa535c9e45938
venu-shetty <venushetty24@gmail.com>
2026-03-09 11:54:11 +0530

    update

--- models/productModel.js
+++ models/productModel.js
@@ -1,7 +1,9 @@
 import mongoose from "mongoose"
 const productSchema = mongoose.Schema({
   name:{type:String,required:true},
+  desc:{type:String,required:true},
   price:{type:Number,required:true},
+   imageUrl: { type: String },
 });
 const productModel = mongoose.model("products",productSchema);
 
