8bc5d225f6f3de06d1ef1c49c66c6396ea9a79ef
venu-shetty <venushetty24@gmail.com>
2026-03-10 13:57:52 +0530

    update

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ import expressLayouts from "express-ejs-layouts";
 import session from "express-session";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-// import {productRouter} from "./routes/productRoute.js";
-import { storeRouter } from "./routes/storeRoute.js";
 import dbConnect from "./config/db.js";
+import productRouter from "./routes/productRoute.js";
+import { storeRouter } from "./routes/storeRoute.js";
 const app = express();
-dotenv.config()
+dotenv.config();
 app.use(expressLayouts);
 app.set("view engine", "ejs");
 app.set("views", "views");
+app.set("layout","layout")
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
@@ -24,13 +25,14 @@ app.use(
 
 app.use("/", storeRouter);
 // app.use("/auth", authRouter);
-// app.use("/products", productRouter);
+app.use("/products", productRouter);
 // app.use("/users", userRouter);
 
 const startServer = async () => {
   await dbConnect();
   app.listen(5000, () => {
     console.log("Server Started");
-});
+  });
 };
+
 startServer()
