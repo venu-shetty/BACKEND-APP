3ac0f0a471dc3a47bd97568c8f95238e868075bf
venu-shetty <venushetty24@gmail.com>
2026-03-12 12:04:33 +0530

    update

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,5 +1,4 @@
-
-import {login,validateUser,register,registerUser,logout} from "../controllers/authController.js"
+import {login,validateUser,register,registerUser,logout,signup,signin} from "../controllers/authController.js"
 import express from "express"
 const authRouter = express.Router()
 
@@ -8,5 +7,7 @@ authRouter.post("/login",validateUser)
 authRouter.get("/register",register)
 authRouter.post("/register",registerUser)
 authRouter.get("/logout",logout)
+authRouter.post("/signup",signup)
+authRouter.post("/signin",signin)
 
-export default authRouter;
+export default authRouter
