414dcc55eb5b6338dde31998f6358616fb803741
venu-shetty <venushetty24@gmail.com>
2026-03-13 11:48:10 +0530

    updated

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ import expressLayouts from "express-ejs-layouts";
 import session from "express-session";
 import cors from "cors";
 import dotenv from "dotenv";
-import mongoose from "mongoose";
+import { authenticateAdmin,authenticateUser } from "./middleware/auth.js";
 import dbConnect from "./config/db.js";
-import { authenticateAdmin } from "./middleware/auth.js";
-
 import productRouter from "./routes/productRoute.js";
 import storeRouter from "./routes/storeRoute.js";
 import homeRouter from "./routes/homeRoute.js";
@@ -14,11 +12,9 @@ import authRouter from "./routes/authRoute.js";
 import userRouter from "./routes/userRoute.js";
 import orderRouter from "./routes/orderRoute.js";
 
-dotenv.config();
 const app = express();
-
-// Middleware
 app.use(cors());
+dotenv.config();
 app.use(expressLayouts);
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -31,8 +27,8 @@ app.use(
   session({
     secret: "secretkey",
     resave: false,
-    saveUninitialized: false
-  })
+    saveUninitialized: false,
+  }),
 );
 
 app.use((req, res, next) => {
@@ -40,19 +36,18 @@ app.use((req, res, next) => {
   next();
 });
 
-// Routes
 app.use("/auth", authRouter);
 app.use("/store", storeRouter);
+app.use("/orders",authenticateUser,orderRouter)
 app.use("/", authenticateAdmin, homeRouter);
 app.use("/products", authenticateAdmin, productRouter);
 app.use("/users", authenticateAdmin, userRouter);
-app.use("/orders", authenticateAdmin, orderRouter); // EJS + API
 
-// Connect DB and start server
+
 const startServer = async () => {
   await dbConnect();
   app.listen(5000, () => {
-    console.log("Server Started on http://localhost:5000");
+    console.log("Server Started");
   });
 };
 
